edc041e329875c287c693620d885c5494c2e4358
Fenhl <fenhl@fenhl.net>
2025-04-17 23:52:18 +0000

    index: Basic styling

--- src/main.rs
+++ src/main.rs
@@ -60,7 +60,7 @@ async fn index(supervisor: &State<Supervisor>) -> Result<RawHtml<String>, superv
                 title : "Mido's House Status";
                 meta(name = "viewport", content = "width=device-width, initial-scale=1, shrink-to-fit=no");
                 //TODO favicon (Lens of Truth?)
-                //TODO CSS
+                style : RawHtml(include_str!("../assets/common.css"));
             }
             body {
                 p {
